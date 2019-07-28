5d379262b0cbb2785241863a2915c7c31ec042bf
Pawel <jarrodek@gmail.com>
2019-07-28 12:54:43 -0700

    test: updating SL test name

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -5,7 +5,7 @@ const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
   const cnf = slSettings();
-  cnf.sauceLabs.testName = 'anypoint-signin';
+  cnf.sauceLabs.testName = 'content-type-selector';
   cnf.browsers = [
     'SL_Chrome',
     'SL_Chrome-1',
